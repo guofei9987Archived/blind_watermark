3b3c36a92f654c7e7ef067e04a4b76387e1d98ca
guofei <guofei9987@foxmail.com>
2020-08-08 03:10:57 +0800

    旋转攻击

--- examples/旋转攻击.py
+++ examples/旋转攻击.py
@@ -2,7 +2,7 @@ from blind_watermark import att
 
 # 旋转攻击
 att.rot_att('output/打上水印的图.png', 'output/旋转攻击.png', angle=45)
-att.rot_att('output/旋转攻击.png', '旋转攻击_还原.png', angle=-45)
+att.rot_att('output/旋转攻击.png', 'output/旋转攻击_还原.png', angle=-45)
 
 # %%提取水印
 from blind_watermark import WaterMark
