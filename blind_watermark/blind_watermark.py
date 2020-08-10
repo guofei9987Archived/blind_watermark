234a399b2050e85334cdd94d90ebda4ae0b30f92
guofei <guofei9987@foxmail.com>
2020-08-10 22:42:20 +0800

    update

--- blind_watermark/blind_watermark.py
+++ blind_watermark/blind_watermark.py
@@ -1,7 +1,7 @@
 import numpy as np
+import copy
 import cv2
 from pywt import dwt2, idwt2
-from scipy.stats import pearsonr
 
 
 class WaterMark:
@@ -11,14 +11,18 @@ class WaterMark:
         self.mod, self.mod2 = mod, mod2  # 用于嵌入算法的除数,mod/mod2 越大鲁棒性越强,但输出图片的失真越大
         self.wm_shape = wm_shape  # 水印的大小
 
-        self.ca, self.ca_block = [None]*3, [None]*3
+        # init data
+        self.img, self.img_YUV = None, None  # self.img 是原图，self.img_YUV 对像素做了偶数化（加白）
+        self.ca, self.hvd, = [np.array([])] * 3, [np.array([])] * 3  # dct
+        self.ca_block = [np.array([])] * 3  # 每个 channel 存一个四维 array，代表四维分块后的结果
+        self.ca_part = [np.array([])] * 3  # 四维分块后，有时因不整除而少一部分，self.ca_part 是少一部分之后的 self.ca
 
     def init_block_index(self):
         # 四维分块后的前2个维度：
         block_shape0, block_shape1 = self.ha_block_shape[0], self.ha_block_shape[1]
         self.length = block_shape0 * block_shape1
         print('最多可嵌入{}kb信息，水印含{}kb信息'.format(self.length / 1000, self.wm_size / 1000))
-        assert self.wm_size > self.length, IndexError('水印大小超过图片的容量')
+        assert self.wm_size < self.length, IndexError('水印大小超过图片的容量')
         # self.part_shape 是取整后的ha二维大小,用于嵌入时忽略右边和下面对不齐的细条部分。
         self.part_shape = (block_shape0 * self.block_shape[0], block_shape1 * self.block_shape[1])
         self.block_index = [(i, j) for i in range(block_shape0) for j in range(block_shape1)]
@@ -34,24 +38,16 @@ class WaterMark:
                                           cv2.BORDER_CONSTANT, value=(0, 0, 0))
 
         self.ha_shape = [(i + 1) // 2 for i in self.img_shape]
+
         self.ha_block_shape = (self.ha_shape[0] // self.block_shape[0], self.ha_shape[1] // self.block_shape[1],
                                self.block_shape[0], self.block_shape[1])
         strides = 4 * np.array([self.ha_shape[1] * self.block_shape[0], self.block_shape[1], self.ha_shape[1], 1])
 
-        # for channel in range(3):
-        #     self.ha
-
-        self.ha_Y, self.coeffs_Y = dwt2(self.img_YUV[:, :, 0], 'haar')
-        self.ha_U, self.coeffs_U = dwt2(self.img_YUV[:, :, 1], 'haar')
-        self.ha_V, self.coeffs_V = dwt2(self.img_YUV[:, :, 2], 'haar')
-        self.ha_Y = self.ha_Y.astype(np.float32)
-        self.ha_U = self.ha_U.astype(np.float32)
-        self.ha_V = self.ha_V.astype(np.float32)
-
-        # 转换成4维分块
-        self.ha_Y_block = np.lib.stride_tricks.as_strided(self.ha_Y.copy(), self.ha_block_shape, strides)
-        self.ha_U_block = np.lib.stride_tricks.as_strided(self.ha_U.copy(), self.ha_block_shape, strides)
-        self.ha_V_block = np.lib.stride_tricks.as_strided(self.ha_V.copy(), self.ha_block_shape, strides)
+        for channel in range(3):
+            self.ca[channel], self.hvd[channel] = dwt2(self.img_YUV[:, :, channel], 'haar')
+            # 转为4维度
+            self.ca_block[channel] = np.lib.stride_tricks.as_strided(self.ca[channel].astype(np.float32),
+                                                                     self.ha_block_shape, strides)
 
     def read_ori_img(self, filename):
         self.read_img(filename)
@@ -97,39 +93,27 @@ class WaterMark:
     def embed(self, filename):
         self.init_block_index()
 
-        embed_ha_Y_block = self.ha_Y_block.copy()
-        embed_ha_U_block = self.ha_U_block.copy()
-        embed_ha_V_block = self.ha_V_block.copy()
-
         self.random_dct = np.random.RandomState(self.password_img)
         index = np.arange(self.block_shape[0] * self.block_shape[1])
 
         for i in range(self.length):
             block_idx = self.block_index[i]
             self.random_dct.shuffle(index)
-            embed_ha_Y_block[block_idx] = self.block_add_wm(embed_ha_Y_block[block_idx], index, i)
-            embed_ha_U_block[block_idx] = self.block_add_wm(embed_ha_U_block[block_idx], index, i)
-            embed_ha_V_block[block_idx] = self.block_add_wm(embed_ha_V_block[block_idx], index, i)
+            for channel in range(3):
+                self.ca_block[channel][block_idx] = self.block_add_wm(self.ca_block[channel][block_idx], index, i)
 
-        embed_ha_Y_part = np.concatenate(np.concatenate(embed_ha_Y_block, 1), 1)
-        embed_ha_U_part = np.concatenate(np.concatenate(embed_ha_U_block, 1), 1)
-        embed_ha_V_part = np.concatenate(np.concatenate(embed_ha_V_block, 1), 1)
-
-        embed_ha_Y = self.ha_Y.copy()
-        embed_ha_Y[:self.part_shape[0], :self.part_shape[1]] = embed_ha_Y_part
-        embed_ha_U = self.ha_U.copy()
-        embed_ha_U[:self.part_shape[0], :self.part_shape[1]] = embed_ha_U_part
-        embed_ha_V = self.ha_V.copy()
-        embed_ha_V[:self.part_shape[0], :self.part_shape[1]] = embed_ha_V_part
-
-        # 逆变换回去
-        embed_ha_Y = idwt2((embed_ha_Y.copy(), self.coeffs_Y), "haar")
-        embed_ha_U = idwt2((embed_ha_U.copy(), self.coeffs_U), "haar")
-        embed_ha_V = idwt2((embed_ha_V.copy(), self.coeffs_V), "haar")
+        embed_ca = copy.deepcopy(self.ca)
+        embed_YUV = [np.array([])] * 3
+        for channel in range(3):
+            # 4维分块变2维
+            self.ca_part[channel] = np.concatenate(np.concatenate(self.ca_block[channel], 1), 1)
+            # 4维分块时右边和下边不能整除的长条保留，其余是主体部分，换成 embed 之后的频域的数据
+            embed_ca[channel][:self.part_shape[0], :self.part_shape[1]] = self.ca_part[channel]
+            # 逆变换回去
+            embed_YUV[channel] = idwt2((embed_ca[channel].copy(), self.hvd[channel]), "haar")
 
         # 合并3通道
-        embed_img_YUV = np.stack([embed_ha_Y, embed_ha_U, embed_ha_V], axis=2)
-
+        embed_img_YUV = np.stack(embed_YUV, axis=2)
         # 之前如果不是2的整数，增加了白边，这里去除掉
         embed_img_YUV = embed_img_YUV[:self.img_shape[0], :self.img_shape[1]]
         embed_img = cv2.cvtColor(embed_img_YUV, cv2.COLOR_YUV2BGR)
@@ -138,11 +122,7 @@ class WaterMark:
 
         cv2.imwrite(filename, embed_img)
 
-        print('隐水印嵌入成功，保存到文件 ', filename)
-        for i in range(3):
-            diff, _ = pearsonr(self.img[:, :, i].flatten(), embed_img[:, :, i].flatten())
-            print('通道{}的相似度是{}'.format(i, diff))
-        print('(相似度越接近1越好)')
+        return embed_img
 
     def block_get_wm(self, block, index):
         block_dct = cv2.dct(block)
@@ -160,39 +140,31 @@ class WaterMark:
         return wm
 
     def extract(self, filename, out_wm_name=None, mode='img'):
-        if not self.wm_shape:
-            print("水印的形状未设定")
-            return 0
-        self.wm_size = np.prod(self.wm_shape)
+        assert self.wm_shape, 'wm_shape（水印形状）未指定'
 
+        self.wm_size = np.prod(self.wm_shape)
         self.read_img(filename)
         self.init_block_index()
 
-        extract_wm = np.array([])
         self.random_dct = np.random.RandomState(self.password_img)
 
         index = np.arange(self.block_shape[0] * self.block_shape[1])
+        wm_extract = np.zeros(shape=(3, self.length))  # 3个channel，每个分块提取的水印，全都记录下来
+        wm = np.zeros(shape=self.wm_size)  # 最终提取的水印，是 wm_extract 循环嵌入+3个 channel 的平均
         for i in range(self.length):
             self.random_dct.shuffle(index)
+            for channel in range(3):
+                wm_extract[channel, i] = self.block_get_wm(self.ca_block[channel][self.block_index[i]], index)
 
-            wm_Y = self.block_get_wm(self.ha_Y_block[self.block_index[i]], index)
-            wm_U = self.block_get_wm(self.ha_U_block[self.block_index[i]], index)
-            wm_V = self.block_get_wm(self.ha_V_block[self.block_index[i]], index)
-            wm = round((wm_Y + wm_U + wm_V) / 3)
-
-            if i < self.wm_size:
-                extract_wm = np.append(extract_wm, wm)
-            else:
-                times, ii = i // self.wm_size, i % self.wm_size
-                extract_wm[ii] = (extract_wm[ii] * times + wm) / (times + 1)
+        for i in range(self.wm_size):
+            wm[i] = wm_extract[:, i::self.wm_size].mean()
 
         # 水印提取完成后，解密
-        wm_index = np.arange(extract_wm.size)
+        wm_index = np.arange(self.wm_size)
         self.random_wm = np.random.RandomState(self.password_wm)
         self.random_wm.shuffle(wm_index)
-        extract_wm[wm_index] = extract_wm.copy()
+        wm[wm_index] = wm.copy()
 
         if mode == 'img':
-            cv2.imwrite(out_wm_name, 255 * extract_wm.reshape(self.wm_shape[0], self.wm_shape[1]))
-        else:
-            return extract_wm
+            cv2.imwrite(out_wm_name, 255 * wm.reshape(self.wm_shape[0], self.wm_shape[1]))
+        return wm
